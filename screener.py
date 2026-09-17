7288059a218f924576d6e1a7a5e575d445ac8287
malbaz <57274162+malbaz@users.noreply.github.com>
2026-09-17 13:12:12 +0300

    Add new stock symbols to screener.py

--- screener.py
+++ screener.py
@@ -15,7 +15,7 @@ WATCHLIST = [
     "SHIP", "CWCO", "BNAI", "AISP", "SNDL", "KULR",
     "RIG", "GTEC", "AMIX", "ADXN", "RETO", "PDSB", 
     "GENVR", "RTB", "WATT", "ALMU", "MEDS", "IREG", 
-    "SOFI", "SNAP"
+    "SOFI", "SNAP", "DAIC", "KXIN", "AEHL",
 ]
 
 def send_telegram_alert(symbol, name, price, volume, change_percent):
