7297e99da7a0228c9971c3297ad4f91ac1b8c75d
malbaz <57274162+malbaz@users.noreply.github.com>
2026-09-17 11:18:34 +0300

    Add 'AMIX' and 'ADXN' to the watchlist

--- screener.py
+++ screener.py
@@ -13,7 +13,7 @@ WATCHLIST = [
     "VEEA", "FTFT", "SOUN", "BBAI", "LUNR", "SERV",
     "MARK", "CYN", "MULN", "PLTR", "BZFD", "QNST", 
     "SHIP", "CWCO", "BNAI", "AISP", "SNDL", "KULR",
-    "RIG", "GTEC"
+    "RIG", "GTEC", "AMIX", "ADXN"
 ]
 
 def send_telegram_alert(symbol, name, price, volume, change_percent):
