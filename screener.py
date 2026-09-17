7b86e7fba97d9fd051097cafac1c631098fd60d9
malbaz <57274162+malbaz@users.noreply.github.com>
2026-09-17 11:27:43 +0300

    Add 'RETO' to the watchlist

--- screener.py
+++ screener.py
@@ -13,7 +13,7 @@ WATCHLIST = [
     "VEEA", "FTFT", "SOUN", "BBAI", "LUNR", "SERV",
     "MARK", "CYN", "MULN", "PLTR", "BZFD", "QNST", 
     "SHIP", "CWCO", "BNAI", "AISP", "SNDL", "KULR",
-    "RIG", "GTEC", "AMIX", "ADXN"
+    "RIG", "GTEC", "AMIX", "ADXN", "RETO"
 ]
 
 def send_telegram_alert(symbol, name, price, volume, change_percent):
